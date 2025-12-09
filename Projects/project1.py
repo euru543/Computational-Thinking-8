6d32c27fa3e7ef0892e2de008aef1a44f12b78da
euru543 <edomiastibebu@seattleacademy.org>
2025-12-09 18:35:37 +0000

    Project 1

--- Projects/project1.py
+++ Projects/project1.py
@@ -4,3 +4,19 @@ print(f"What do you like to do in your free time")
 action = input()
 
 print(f"That's pretty cool")
+
+print(f"My favorite show is family reunion")
+print(f"What's Yours?")
+show = input()
+
+print(f"That's pretty tuff")
+
+print(f"I have a list of my favorite musical artists")
+print(f"Who's your favorite artist or artists? ")
+favorite = input()
+
+print(f"You have good taste twin")
+
+print(f"It's time for me to go")
+print(f"Peace")
+response = input()
